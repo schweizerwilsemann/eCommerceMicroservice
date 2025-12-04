a33720ec4e68a8a787fa3f5bf07dccb7f6d4a4af
KhoaND <khoanguyenduc99@gmail.com>
2025-12-04 09:20:51 +0700

    add new middleware

---
+++ eCommerce.SharedLibrary/eCommerce.SharedLibrary/Middleware/ListenToOnlyApiGateway.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eCommerce.SharedLibrary.Middleware
+{
+    public class ListenToOnlyApiGateway (RequestDelegate next)
+    {
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            // extract the specific header from the request
+
+            var signedHeader = context.Request.Headers["Api-Gateway"];
+
+            // null means, the request is not coming frm the api gateway
+            if (signedHeader.FirstOrDefault() is null)
+            {
+                context.Response.StatusCode = StatusCodes.Status503ServiceUnavailable;
+                await context.Response.WriteAsync("Service is unavailable");
+                return;
+            }
+            else
+            {
+                await next(context);
+            }
+        }
+    }
+}
