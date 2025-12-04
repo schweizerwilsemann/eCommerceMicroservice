021b8018b3f088119be40671de2f0125c15ec8b6
KhoaND <khoanguyenduc99@gmail.com>
2025-12-04 09:21:25 +0700

    add global exception

---
+++ eCommerce.SharedLibrary/eCommerce.SharedLibrary/Middleware/GlobalException.cs
@@ -0,0 +1,81 @@
+﻿using eCommerce.SharedLibrary.Logs;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using Microsoft.IdentityModel.Tokens;
+using System.Net;
+using System.Text.Json;
+
+namespace eCommerce.SharedLibrary.Middleware
+{
+    public class GlobalException (RequestDelegate next)
+    {
+        public async Task InvokeAsync(HttpContext context)
+        {
+            string message = "Internal Server Error occurred.";
+
+            int statusCode = (int)HttpStatusCode.InternalServerError;
+
+            string title = "Error";
+
+            try
+            {
+                await next(context);
+
+                // too many requests - status code: 429
+                if (context.Response.StatusCode == StatusCodes.Status429TooManyRequests)
+                {
+                    title = "Warning";
+                    message = "Too many requests";
+                    statusCode = (int)StatusCodes.Status429TooManyRequests;
+                    await ModifyHeader(context, title, message, statusCode);
+                }
+
+                // unauthorized  - status code  : 401
+                if (context.Response.StatusCode == StatusCodes.Status401Unauthorized)
+                {
+                    title = "Alert";
+                    message = "You are not authorized to access.";
+                    statusCode = (int)StatusCodes.Status401Unauthorized;
+                    await ModifyHeader(context, title, message, statusCode);
+                }
+
+                // Forbidden - status code : 403 
+                if (context.Response.StatusCode == StatusCodes.Status403Forbidden)
+                {
+                    title = "Out of access";
+                    message = "You are not allowed/required to access";
+                    statusCode = (int)StatusCodes.Status403Forbidden;
+                    await ModifyHeader(context, title, message, statusCode);
+                }
+            } catch(Exception ex)
+            {
+                // Log original exception
+                LogExceptions.LogException(ex);
+                // if time out  - status code : 408 timeout
+                if (ex is TaskCanceledException || ex is TimeoutException)
+                {
+                    title = "Out of time";
+                    message = "Request timeout ... try again";
+                    statusCode = StatusCodes.Status408RequestTimeout;
+                }
+
+                // if none of the exception - throw a default
+                await ModifyHeader(context, title, message, statusCode);
+            }
+
+        }
+
+        public async Task ModifyHeader(HttpContext context, string title, string message, int statusCode)
+        {
+            context.Response.ContentType = "application/json";
+            await context.Response.WriteAsync(JsonSerializer.Serialize(new ProblemDetails()
+            {
+                Detail = message,
+                Status = statusCode,
+                Title = title
+            }), CancellationToken.None);
+            return; 
+        }
+    }
+}
