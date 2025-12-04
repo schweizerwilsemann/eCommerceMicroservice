e333233add1a5c54bd72c89553eff42635c52d28
KhoaND <khoanguyenduc99@gmail.com>
2025-12-04 09:21:52 +0700

    add generic interface

---
+++ eCommerce.SharedLibrary/eCommerce.SharedLibrary/Interfaces/IGenericInterface.cs
@@ -0,0 +1,19 @@
+﻿using eCommerce.SharedLibrary.Responses;
+using System.Linq.Expressions;
+
+namespace eCommerce.SharedLibrary.Interfaces
+{
+    public interface IGenericInterface<T> where T : class
+    {
+        Task<Response> CreateAsync(T entity);
+        Task<Response> UpdateAsync(T entity);
+        Task<Response> DeleteAsync(T entity);
+
+        Task<IEnumerable<T>> GetAllAsync();
+        Task<T?> GetByIdAsync(int id);
+        Task<T?> FindByIdAsync(int id);
+
+        Task<T?> GetByAsync(Expression<Func<T, bool>> predicate);
+
+    }
+}
