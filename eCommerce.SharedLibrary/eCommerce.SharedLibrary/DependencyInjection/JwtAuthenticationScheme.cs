0814e973d63928bdab05d339ca13a1e41f89a0c8
KhoaND <khoanguyenduc99@gmail.com>
2025-12-04 09:23:02 +0700

    add jwt authentication scheme - DI

---
+++ eCommerce.SharedLibrary/eCommerce.SharedLibrary/DependencyInjection/JwtAuthenticationScheme.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System.Text;
+
+namespace eCommerce.SharedLibrary.DependencyInjection
+{
+    public static class JwtAuthenticationScheme
+    {
+        public static IServiceCollection AddJwtAuthenticationScheme(this IServiceCollection services, IConfiguration configuration)
+        {
+
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer("Bearer", options => {
+                var key = Encoding.UTF8.GetBytes(configuration.GetSection("Authentication:Key").Value!);
+                string issuer = configuration.GetSection("Authentication:Issuer").Value!;
+                string audience = configuration.GetSection("Authentication:Audience").Value!;
+
+                options.RequireHttpsMetadata = false;
+                options.SaveToken = true;
+                options.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    ValidIssuer = issuer,
+                    ValidAudience = audience,
+                    IssuerSigningKey = new Microsoft.IdentityModel.Tokens.SymmetricSecurityKey(key),
+                    ClockSkew = TimeSpan.Zero
+                };
+            });
+            return services;
+        }
+    }
+}
