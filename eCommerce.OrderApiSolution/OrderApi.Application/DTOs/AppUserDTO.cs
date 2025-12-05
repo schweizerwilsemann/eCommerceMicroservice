4807d8d9e6e1ff356309eba2cabebbf8e59cc772
KhoaND <khoanguyenduc99@gmail.com>
2025-12-05 15:27:22 +0700

    add AppUserDTO record for user data transfer

---
+++ eCommerce.OrderApiSolution/OrderApi.Application/DTOs/AppUserDTO.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace OrderApi.Application.DTOs
+{
+    public record AppUserDTO(
+        int Id,
+        [Required]
+        string Name,
+        [Required]
+        string TelephoneNumber,
+        [Required, EmailAddress]
+        string Email,
+        [Required, EmailAddress]
+        string Address,
+        [Required]
+        string Password,
+        [Required]
+        string Role
+    );
+}
